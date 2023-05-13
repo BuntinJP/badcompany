064b44d25cca4602b6d8ea88f444d95b51703fb8
Buntin-Emilico <mail@buntin.tech>
2023-05-14 06:08:12 +0900

    chorima

---
+++ README.md
@@ -0,0 +1,9 @@
+# badcomapny
+
+## this is a manga eater client for discord.
+
+using Cloudflare Workers
+
+Edge Computing & Mangarawjp scraper practice.
+
+
---
+++ src/commands.js
@@ -0,0 +1,14 @@
+/**
+ * discordに登録するコマンドの集合
+ */
+
+export const commands= [
+  {
+    name: "ping",
+    description: "ping pong"
+  },
+  {
+    name: "help",
+    description: "ヘルプを表示します"
+  },
+]
